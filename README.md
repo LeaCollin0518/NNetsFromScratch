0e056fd4c06a971765a36cecbda582ed5d8c9d71
stumash <stuart.mashaal@gmail.com>
2019-01-17 22:28:09 -0500

    finish testing VanillaNet using MNIST data

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Neural Networks From Scratch
+
+## `vanilla_net.py` and `test_vanilla_net.py`
+
+`vanilla_net.py` contains a `numpy`-only implementation of multi-layer perceptron. This neural net implementation is based on [this article](https://mattmazur.com/2015/03/17/a-step-by-step-backpropagation-example/) and tested partially based on data and targets from that article as well.
+
+`test_vanilla_net.py` contains tests for the neural net. Two of the tests are 'marked' with `pytest.mark.slow` and `pytest.mark.very_slow`, respectively. To run all tests **except** for the `slow` and `very_slow` ones, run
+
+```
+$ pytest -m "not slow and not very_slow"
+```
+
+Otherwise, to run all tests, just run
+
+```
+$ pytest
+```
--- test_vanilla_net.py
+++ test_vanilla_net.py
@@ -1,8 +1,11 @@
 from vanilla_net import VanillaNet, mse
 import numpy as np
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.datasets import load_digits # MNIST
+import pytest
 
 def test_feed_forward():
-    nn, datum, target = setup()
+    nn, datum, target = setup_MattMazur()
 
     # test results
     actual_result   = nn.feed_forward(datum)
@@ -10,7 +13,7 @@ def test_feed_forward():
     assert((actual_result == expected_result).all())
 
 def test_backprop_single_row():
-    nn, datum, target = setup()
+    nn, datum, target = setup_MattMazur()
 
     for _ in range(10000):
         nn._backprop(datum, target)
@@ -19,10 +22,9 @@ def test_backprop_single_row():
     epsilon, error =  0.05, mse(actual_result, target)
     assert(error < epsilon)
 
-# data,targets = tuple(map(lambda row: np.repeat(row, 10000, axis=0), (datum,target)))
-
+@pytest.mark.slow
 def test_train():
-    nn, datum, target = setup()
+    nn, datum, target = setup_MattMazur()
 
     data, targets = tuple(map(lambda M: np.repeat(M, 10000, axis=0), (datum, target)))
     nn.train(data, targets, 1000, 10)
@@ -32,7 +34,36 @@ def test_train():
     epsilon, error =  0.05, mse(actual_result, target)
     assert(error < epsilon)
 
-def setup():
+@pytest.mark.very_slow
+def test_MNIST():
+    # get the data
+    digits = load_digits()
+
+    train_data = np.array([img.reshape(64, ) for img in digits.images])
+
+    targets = digits.target.reshape(digits.target.shape[0], 1)
+    targets = LabelBinarizer().fit_transform(targets)
+
+    # initialize the NN
+    nn_dims = [train_data.shape[1], 100, 50, targets.shape[1]]
+    nn = VanillaNet(nn_dims, learning_rate=0.1)
+
+    # train the net. 180 batch of 100 imgs each. 500 epochs
+    errs = nn.train(train_data, targets, 180, 500)
+
+    # calculate accuracy
+    accuracy = 0
+    for img, target in zip(train_data, digits.target):
+        prediction = np.argmax(nn.feed_forward(img.reshape(1, 64)))
+        if prediction == target: accuracy += 1
+    accuracy /= train_data.shape[0]
+
+    assert(accuracy > .99)
+
+def setup_MattMazur():
+    """
+    Neural net from https://mattmazur.com/2015/03/17/a-step-by-step-backpropagation-example/
+    """
     # initialize nn
     nn = VanillaNet([2,2,2])
     w1 = np.array([[0.15, 0.20], [0.25, 0.30], [0.35, 0.35]])
